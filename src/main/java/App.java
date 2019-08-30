20ea86343bd9555240c06f8591fc02bbc045d984
Fried.Ice <fried.ice.94@gmail.com>
2019-08-30 22:23:22 +0200

    Add source skeleton

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+/**
+ * Main class.
+ * Set up the application and run it.
+ */
+public final class App {
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(final String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
